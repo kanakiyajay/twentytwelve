6af30670dc3cb934c0f65a01c872cf7bb6b1a184
Jay Kanakiya <kanakiyajay@yahoo.com>
2012-10-20 20:56:11 +0530

    flash slider

--- page-templates/forhome.php
+++ page-templates/forhome.php
@@ -5,74 +5,79 @@
  * Description: Twenty Twelve loves the no-sidebar look as much as
  * you do. Use this page template to remove the sidebar from any page.
  *
- * Tip: to remove the sidebar from all posts and pages simply remove
- * any active widgets from the Main Sidebar area, and the sidebar will
- * disappear everywhere.
- *
- * @package WordPress
- * @subpackage Twenty_Twelve
- * @since Twenty Twelve 1.0
  */
 get_header(); ?>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
         <div class="row">
-            <div class="col span_1_of_1" id="s-wrapper">                
-            <div id="registercta">Register</div>
-            <img class="prev" src="<?php echo get_template_directory_uri(); ?>/images/bt-prev.png" alt="Previous Frame" />
-            <img class="next" src="<?php echo get_template_directory_uri(); ?>/images/bt-next.png" alt="Next Frame" />
-            <div id="maincta">
-                <ul>
-                    <li id="slide1">
-                       <div class="info1 insideslide">        
-                            <h2>25,000 Footfalls<br>
-                                10,000 Participants
-                            </h2>
-                        </div> 
-                        <!--<img src="images/default.jpg" class="default animate-in" alt="Deafult Image">-->
-                    </li>
-                    <li id="slide2">
-                       <div class="info2 insideslide">                     
+            <div class="col span_1_of_1" id="s-wrapper">
+                <div id="registercta">
+                    Register
+                </div>
+                <img class="prev" src="<?php echo get_template_directory_uri();?>/images/bt-prev.png" alt="Previous Frame">
+                 <img class="next" src="<?php echo get_template_directory_uri();?>/images/bt-next.png" alt="Next Frame">
+                <div id="maincta">
+                    <ul>
+                        <li id="slide1">
                             
-                            <h2>53 Events <br> 1 Lakh Rupees prizes</h2>
-                        </div> 
-                    </li>
-                    <li id="slide3">
-                       <div class="info3 insideslide">                          
+                                <h2 class="title1">
+                                   <span class="flash1">Thoughts</span>.<span class="flash3">Passion</span>.<span class="flash2">Revolution</span>
+                                </h2>
                             
-                            <h2>Description 3</h2>
-                        </div> 
-                    </li>
-                </ul>
-            </div>
+                        </li>
+                        <li id="slide2">
+                            
+                                <h2 class="title1">
+                                    25,000 Footfalls
+                                </h2>
+                                <h2 class="title2">
+                                    10,000 Participants
+                                </h2>
+                            <!--<img src="images/default.jpg" class="default animate-in" alt="Deafult Image">-->
+                        </li>
+                        <li id="slide3">                            
+                                <h2 class="title1">
+                                    53 Events
+                                </h2>
+                                <h2 class="title2">
+                                    1 Lakh Rupees prizes
+                                </h2>                            
+                        </li>
+                    </ul>
+                </div>
             </div>
             <div class="clearfix"></div>
         </div>
         <div class="row">
             <div class="col span_1_of_3">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/placeholder.png" alt="" class="windows">
+                <img src="<?php echo get_template_directory_uri();?>/images/placeholder.png" alt="" class="windows">
             </div>
             <div class="col span_1_of_3">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/placeholder.png" alt="" class="windows">
+                <img src="<?php echo get_template_directory_uri();?>/images/placeholder.png" alt="" class="windows">
             </div>
             <div class="col span_1_of_3">
                 <div class="subrow">
                     <div class="col span_1_of_2">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/placeholder.png" alt="" class="windows">
+                        <img src="<?php echo get_template_directory_uri();?>/images/placeholder.png" alt="" class="windows">
                     </div>
                     <div class="col span_1_of_2">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/placeholder.png" alt="" class="windows">
+                        <img src="<?php echo get_template_directory_uri();?>/images/placeholder.png" alt="" class="windows">
                     </div>
                 </div>
                 <div class="subrow">
                     <div class="col span_1_of_2">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/placeholder.png" alt="" class="windows">
+                        <img src="<?php echo get_template_directory_uri();?>/images/placeholder.png" alt="" class="windows">
                     </div>
                     <div class="col span_1_of_2">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/placeholder.png" alt="" class="windows">
+                        <img src="<?php echo get_template_directory_uri();?>/images/placeholder.png" alt="" class="windows">
                     </div>
                 </div>
             </div>
         </div>
-
-        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.8.2.min.js"></script>
-        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/sequence.jquery-min.js"></script>
-<?php get_footer(); ?>
+<script type="text/javascript" src="<?php echo get_template_directory_uri();?>/js/sequence.jquery-min.js">
+</script><?php get_footer(); ?>
+    </body>
+</html>
