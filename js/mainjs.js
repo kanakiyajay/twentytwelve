b0fe2dc485d848cfc433a37a9a69e1009af5ece2
Jay Kanakiya <kanakiyajay@yahoo.com>
2012-10-21 13:32:30 +0530

    added masonry

--- js/mainjs.js
+++ js/mainjs.js
@@ -7,9 +7,33 @@ jQuery(document).ready(function ($) {
     autoPlay : true,
     autoPlayDelay : 100,
     animateStartingFrameIn: true,
-    preloadTheseFrames: [1]
+    preloadTheseFrames: [1],
+    nextButton : true,
+    prevButton:true
 
   }
   var sequence = $("#maincta").sequence().data("sequence");
   console.log(sequence);
-});
+  $('.prev').on('click',function () {
+  	sequence.prev();
+  });
+  $('.next').on('click',function () {
+  	sequence.next();
+  });
+
+  //For Masonry
+  $('#masonryrow').masonry({
+    // options
+    itemSelector : '.events',
+    columnWidth : 240
+  });
+
+  //For the Hover Effect
+$(".events").hover(
+  function () {
+    mouseenter
+  },
+  function () {
+    mouseleave
+  }
+);
--- page-templates/forhome.php
+++ page-templates/forhome.php
@@ -14,18 +14,18 @@ get_header(); ?>
     <body>
         <div class="row">
             <div class="col span_1_of_1" id="s-wrapper">
-                <div id="registercta">
+                <!--<div id="registercta">
                     Register
-                </div>
-                <img class="prev" src="<?php echo get_template_directory_uri();?>/images/bt-prev.png" alt="Previous Frame">
-                 <img class="next" src="<?php echo get_template_directory_uri();?>/images/bt-next.png" alt="Next Frame">
+                </div>-->
+                <img class="prev hidden" src="<?php echo get_template_directory_uri();?>/images/bt-prev.png" alt="Previous Frame">
+                 <img class="next hidden" src="<?php echo get_template_directory_uri();?>/images/bt-next.png" alt="Next Frame">
                 <div id="maincta">
                     <ul>
                         <li id="slide1">
-                            
-                                <h2 class="title1">
-                                   <span class="flash1">Thoughts</span>.<span class="flash3">Passion</span>.<span class="flash2">Revolution</span>
-                                </h2>
+                                <h2 class="flash1 animate-in">Thoughts.</h2>
+                                <h2 class="flash2 animate-in">Passion.</h2>
+                                <h2 class="flash3 animate-in">Revolution</h2>
+                            <!--<img src="images/default.jpg" class="default animate-in" alt="Deafult Image">-->
                             
                         </li>
                         <li id="slide2">
@@ -36,7 +36,6 @@ get_header(); ?>
                                 <h2 class="title2">
                                     10,000 Participants
                                 </h2>
-                            <!--<img src="images/default.jpg" class="default animate-in" alt="Deafult Image">-->
                         </li>
                         <li id="slide3">                            
                                 <h2 class="title1">
@@ -51,33 +50,20 @@ get_header(); ?>
             </div>
             <div class="clearfix"></div>
         </div>
-        <div class="row">
-            <div class="col span_1_of_3">
-                <img src="<?php echo get_template_directory_uri();?>/images/placeholder.png" alt="" class="windows">
-            </div>
-            <div class="col span_1_of_3">
-                <img src="<?php echo get_template_directory_uri();?>/images/placeholder.png" alt="" class="windows">
-            </div>
-            <div class="col span_1_of_3">
-                <div class="subrow">
-                    <div class="col span_1_of_2">
-                        <img src="<?php echo get_template_directory_uri();?>/images/placeholder.png" alt="" class="windows">
-                    </div>
-                    <div class="col span_1_of_2">
-                        <img src="<?php echo get_template_directory_uri();?>/images/placeholder.png" alt="" class="windows">
-                    </div>
-                </div>
-                <div class="subrow">
-                    <div class="col span_1_of_2">
-                        <img src="<?php echo get_template_directory_uri();?>/images/placeholder.png" alt="" class="windows">
-                    </div>
-                    <div class="col span_1_of_2">
-                        <img src="<?php echo get_template_directory_uri();?>/images/placeholder.png" alt="" class="windows">
-                    </div>
-                </div>
-            </div>
+        <div class="row" id="masonryrow">
+            <div class="events small"><img src="<?php echo get_template_directory_uri();?>/images/call.jpg " alt=""></div>
+            <div class="events big"><img src="<?php echo get_template_directory_uri();?>/images/placeholder.png" alt=""></div>
+            <div class="events small"><img src="<?php echo get_template_directory_uri();?>/images/call.jpg " alt=""></div>
+            <div class="events small"><img src="<?php echo get_template_directory_uri();?>/images/call.jpg " alt=""></div>
+            <div class="events big"><img src="<?php echo get_template_directory_uri();?>/images/placeholder.png" alt=""></div>
+            <div class="events small"><img src="<?php echo get_template_directory_uri();?>/images/call.jpg " alt=""></div>
+            <div class="events big"><img src="<?php echo get_template_directory_uri();?>/images/placeholder.png" alt=""></div>
+            <div class="events small"><img src="<?php echo get_template_directory_uri();?>/images/call.jpg " alt=""></div>
+            <div class="events small"><img src="<?php echo get_template_directory_uri();?>/images/call.jpg " alt=""></div>
+            <div class="events big"><img src="<?php echo get_template_directory_uri();?>/images/placeholder.png" alt=""></div>
         </div>
-<script type="text/javascript" src="<?php echo get_template_directory_uri();?>/js/sequence.jquery-min.js">
-</script><?php get_footer(); ?>
+<script type="text/javascript" src="<?php echo get_template_directory_uri();?>/js/jquery.masonry.min.js"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri();?>/js/sequence.jquery-min.js"></script>
+<?php get_footer(); ?>
     </body>
 </html>
