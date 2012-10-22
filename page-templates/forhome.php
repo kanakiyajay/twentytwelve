3f5631f26eca11ac6af6dd5e2a2ecc2652fe7b27
Jay Kanakiya <kanakiyajay@yahoo.com>
2012-10-22 16:40:07 +0530

    'notimportant'

--- page-templates/forhome.php
+++ page-templates/forhome.php
@@ -50,6 +50,32 @@ get_header(); ?>
             </div>
             <div class="clearfix"></div>
         </div>
+      <?php if (have_posts()) : ?>
+<div id="post-area">
+<?php while (have_posts()) : the_post(); ?> 
+
+        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+         <?php if ( has_post_thumbnail() ) { ?>
+         <div class="gridly-image"><a href="<?php the_permalink() ?>"><?php the_post_thumbnail( 'summary-image' );  ?></a></div>
+          <div class="gridly-category"><p><?php the_category(', ') ?></p></div>
+       
+          <?php } ?>
+                <div class="gridly-copy"><h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+                <p class="gridly-date"><?php the_time(get_option('date_format')); ?>  </p>
+
+<?php the_excerpt(); ?> 
+
+               <p class="gridly-link"><a href="<?php the_permalink() ?>">View more &rarr;</a></p>
+         </div>
+       </div>
+       
+       
+
+<?php endwhile; ?>
+</div>
+<?php else : ?>
+<?php endif; ?>
+
         <div class="row" id="masonryrow">
             <div class="events small"><img src="<?php echo get_template_directory_uri();?>/images/call.jpg " alt=""></div>
             <div class="events big"><img src="<?php echo get_template_directory_uri();?>/images/placeholder.png" alt=""></div>
